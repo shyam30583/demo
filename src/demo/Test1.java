1b3a4ae53ab0be0056b97c534826fb1044f1578e
SamSundar <SamSundar@LAPTOP-UR936062>
2022-06-22 06:02:59 +0530

    second commit

--- src/demo/Test1.java
+++ src/demo/Test1.java
@@ -5,6 +5,9 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Test1");
+for(int i=0;i<10;i++) {
+	System.out.println("Test1 "+i);
+}
 	}
 
 }
