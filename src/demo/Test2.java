751bcafeebe9c98d7b255618bbb93b40cf4f2183
SamSundar <SamSundar@LAPTOP-UR936062>
2022-06-22 06:37:40 +0530

    pre-prod commit

---
+++ src/demo/Test2.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Test2");
+for(int i=0;i<100;i++) {
+	System.out.println("Test2 "+i);
+}
+	}
+
+}
